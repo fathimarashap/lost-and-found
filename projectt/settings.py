4f4b924f6a4b597989bcfd595b1b0f1ec882b623
fathimarashap <ishyarasha.24@gmail.com>
2026-05-21 15:43:53 +0530

    cloudinary URL

--- projectt/settings.py
+++ projectt/settings.py
@@ -95,7 +95,5 @@ cloudinary.config(
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
-    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
-    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+    'CLOUDINARY_URL': os.environ.get('CLOUDINARY_URL'), 
 }
